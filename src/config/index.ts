81e12b0f886f05b97849b5a5ca8aa31e80296677
sraveshnandan <kumarsravesh39@gmail.com>
2024-04-02 10:59:11 +0530

    db error fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Port = process.env.PORT;
-const MongoDbUri = process.env.MONGODB_URI_LOCAL;
+const MongoDbUri = process.env.MONGODB_URI;
 const StatusSecret = process.env.STATUS_SECRET;
 const JWT_SECRET =process.env.JWT_PRIVATE_KEY;
 
